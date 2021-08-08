28d1a03c30457580d38751f4666655197d9f46c9
Renan Carlos Agostinho <renan.carlos.agostinho@gmail.com>
2021-08-08 19:01:06 -0300

    adicionada anotação no CarRepository

--- src/main/java/com/springboot/repositories/CarRepository.java
+++ src/main/java/com/springboot/repositories/CarRepository.java
@@ -1,9 +1,11 @@
 package com.springboot.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.springboot.entities.Car;
 
+@Repository
 public interface CarRepository extends JpaRepository<Car, Long> {
 
 }
